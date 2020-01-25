538f6abebbdc0679ac5fb7815f8669f29adace03
roz-Devops <55594261+roz-Devops@users.noreply.github.com>
2020-01-25 22:28:15 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
- export AWS_ACCESS_KEY_ID=$(aws configure get aws_access_key_id)
- export AWS_SECRET_ACCESS_KEY=$(aws configure get aws_secret_access_key)
+ export ACCESS_KEY_ID=$(aws configure get aws_access_key_id)
+ export SECRET_ACCESS_KEY=$(aws configure get aws_secret_access_key)
 
-echo 'export AWS_ACCESS_KEY_ID='$AWS_ACCESS_KEY_ID
-echo ${AWS_ACCESS_KEY_ID}
-
-echo 'export AWS_SECRET_ACCESS_KEY='$AWS_SECRET_ACCESS_KEY 
-echo ${AWS_SECRET_ACCESS_KEY}
+echo 'export AWS_ACCESS_KEY_ID='$ACCESS_KEY_ID >> .env
+echo 'export AWS_SECRET_ACCESS_KEY='$SECRET_ACCESS_KEY >> .env
 
 
-docker-compose up
